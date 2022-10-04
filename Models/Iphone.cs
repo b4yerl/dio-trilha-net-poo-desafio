8b383e175b7806ccb51cb451ac3502f2ef134c7e
b4yerl <gcbayerlc@gmail.com>
2022-10-04 19:57:14 -0300

    Implementada a solução do desafio

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+  // TODO: Herdar da classe "Smartphone"
+  public class Iphone : Smartphone
+  {
+    public Iphone(string number, string model, string imei, int storage) : base(number, model, imei, storage)
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+
     }
+
+    // TODO: Sobrescrever o método "InstalarAplicativo"
+    public override void InstalarAplicativo(string nomeApp)
+    {
+      Console.WriteLine($"Instalando o aplicativo {nomeApp} no seu Iphone...");
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Testes Nokia:");
+Nokia nokia = new Nokia(number: "123-456-789", model:"5.4", imei: "1111111111", storage: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Binance");
+
+Console.WriteLine("Testes Iphone:");
+Iphone iphone = new Iphone(number: "987-654-321", model: "11 Pro Max", imei: "2222222222", storage: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("GitHub");
